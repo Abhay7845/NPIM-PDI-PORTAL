923be984fbea5216164dc304aefa56f61c24409a
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2025-01-20 09:54:55 +0530

    updated

--- src/HostManager/UrlManager.jsx
+++ src/HostManager/UrlManager.jsx
@@ -1,4 +1,4 @@
-export const HOST_URL = "https://tanishqdigitalnpim.titan.in:8443/PNPIM";
+export const HOST_URL = "https://digital.titan.in:8443/PNPIM";
 export const INDENT_HOST_URL = "https://tanishqdigitalnpim.titan.in:8443/IndentExpress";
 
 export const excelsheetURL = "https://docs.google.com/spreadsheets/d/1AoThWIV-h0xRdn1ONW_qABM_CvIsVicBx2JiehwODeA/edit#gid=0";
