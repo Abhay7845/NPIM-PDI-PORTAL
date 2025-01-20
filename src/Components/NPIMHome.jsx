f8201edac45f0ab9181235c30132a948072b8ff7
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2025-01-20 10:09:13 +0530

    bg chenged

--- src/Components/NPIMHome.jsx
+++ src/Components/NPIMHome.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../Style/CssStyle/NPIMHome.css";
 import PreLoginHeader from "../Components/PreLoginHeader";
-import BGImage from "../images/NpimBanner.png"
+import BGImage from "../images/LoginBg.jpg"
 import Login from "../pages/Login";
 import Loader from "./Loader";
 import { APIGetStatusPortal } from "../HostManager/CommonApiCallL3";
